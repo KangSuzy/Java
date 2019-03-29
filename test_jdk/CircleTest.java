8d5359d1449bb76dcf4af798e5550a208f730b16
KangSuzy <astnwl321@gmail.com>
2019-03-29 14:24:47 +0900

    practice java examples

---
+++ test_jdk/CircleTest.java
@@ -0,0 +1,15 @@
+class Circle{
+ 	int r;
+ 	double PI = 3.14;
+}
+public class CircleTest{
+	public static void main(String args[]){
+	Circle c1 = new Circle();
+	c1.r = 10;
+	System.out.println("c1의 둘레: " + c1.r*2*c1.PI + "c1의 넓이:" + c1.r*c1.r*c1.PI);
+	
+	Circle c2 = new Circle();
+	c2.r = 20;
+	System.out.println("c2의 둘레: " + c2.r*2*c2.PI + "c2의 넓이:" + c2.r*c2.r*c2.PI);
+	}
+}
