71841c06b80d9f8e4726ad21085f995e157a4645
KangSuzy <astnwl321@gmail.com>
2019-05-06 20:35:38 +0900

    practice Java Array

---
+++ test_jdk/ArrayTest.java
@@ -0,0 +1,12 @@
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] weeks = {"월", "화", "수", "목", "금", "토", "일"};
+		for (int i=0; i <weeks.length; i++) {
+			System.out.print(weeks[i]);
+		}
+	}
+
+}
