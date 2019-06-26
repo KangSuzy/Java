32557379abf9f0896bd3fbf9fb60263afcfe7e6c
KangSuzy <astnwl321@gmail.com>
2019-06-27 02:01:27 +0900

    산술연술자로 총점과 평균구하기

---
+++ test_jdk/Score.java
@@ -0,0 +1,18 @@
+
+public class Score {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int mathScore = 80;
+		int engScore = 100;
+		
+		int totalScore =  mathScore + engScore;
+		System.out.println("ÃÑÁ¡ "+totalScore);
+		
+		double avgScore = totalScore / 2.0;
+		System.out.println("Æò±Õ "+avgScore);
+		
+	}
+
+}
