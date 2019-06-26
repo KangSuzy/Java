352a0185e6621e23f6f0dd61c48076634a1cc434
KangSuzy <astnwl321@gmail.com>
2019-06-27 02:26:15 +0900

    변수 선언과 동시에 초기화하기

---
+++ test_jdk/Variable.java
@@ -0,0 +1,10 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myAge = 22; //myAge 변수 선언과 동시에 초기화하기 !
+		System.out.println(myAge);
+	}
+
+}
