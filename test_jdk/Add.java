0a4180df1174ac61d212a6debe250dfc171bcd24
KangSuzy <astnwl321@gmail.com>
2019-06-27 02:22:35 +0900

    1부터 10까지더하기

---
+++ test_jdk/Add.java
@@ -0,0 +1,20 @@
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int num = 1;
+		 num +=2;
+		 num +=3;
+		 num +=4;
+		 num +=5;
+		 num +=6;
+		 num +=7;
+		 num +=8;
+		 num +=9;
+		 num +=10;
+		 
+		 System.out.println("1부터 10까지 총합은 " + num);
+	}
+
+}
