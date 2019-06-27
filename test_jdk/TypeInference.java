2ce3e7bbab0d32cd4264658a2349ca84f898f7a3
KangSuzy <astnwl321@gmail.com>
2019-06-27 17:44:29 +0900

    자료형 추론

---
+++ test_jdk/TypeInference.java
@@ -0,0 +1,19 @@
+
+public class TypeInference {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10;
+		var j = 10.0;
+		var str = "Hello";
+		
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(str);
+		
+		str = "test"; // var 변하는 변수
+
+		System.out.println(str);
+		
+		}
+}
