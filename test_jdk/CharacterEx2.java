167de13ed88d91969bc7c551247d5f4674099dba
KangSuzy <astnwl321@gmail.com>
2019-06-27 16:43:46 +0900

    문자형 연습2

---
+++ test_jdk/CharacterEx2.java
@@ -0,0 +1,14 @@
+
+public class CharacterEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch1 = '¼ö';
+		char ch2 = '\uC218';
+		
+		System.out.println(ch1);
+		System.out.println(ch2);
+		
+	}
+
+}
