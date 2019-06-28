886d6d94559d93bfa371d21b2bf93eaad2aa729c
KangSuzy <astnwl321@gmail.com>
2019-06-28 18:36:11 +0900

    증가, 감소 연산자로 값 연산하기

---
+++ test_jdk/OperationEx.java
@@ -0,0 +1,15 @@
+
+public class OperationEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int gameScore = 150;
+		
+		int lastScore = ++gameScore;
+		System.out.println(lastScore);
+		
+		int lastScore2 = --gameScore;
+		System.out.println(lastScore2);
+	}
+
+}
