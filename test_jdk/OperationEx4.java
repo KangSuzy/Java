9cc81ade47f35c962e031473645bc8956da973c9
KangSuzy <astnwl321@gmail.com>
2019-06-28 19:00:22 +0900

    비트연산자를 이용하기

---
+++ test_jdk/OperationEx4.java
@@ -0,0 +1,17 @@
+
+public class OperationEx4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 0B00000101;
+		
+		System.out.println(num << 2);
+		System.out.println(num >> 2);
+		System.out.println(num >>> 2);
+		System.out.println(num);
+		
+		num = num << 2;
+		System.out.println(num);
+	}
+
+}
