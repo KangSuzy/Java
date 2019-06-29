175541ed31f729082a6c7a4397c2c5e06913a79d
KangSuzy <astnwl321@gmail.com>
2019-06-29 21:33:01 +0900

    for문 사용하기

---
+++ test_jdk/ForExample.java
@@ -0,0 +1,15 @@
+
+public class ForExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		int sum;
+		for(i=0, sum =0; i<10; i++) {
+			sum += (i+1);
+		}
+		
+		System.out.println(sum);
+	}
+
+}
