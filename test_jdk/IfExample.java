645f1e18c60dfea4f2b049b0b7bda2e9603bdb5d
KangSuzy <astnwl321@gmail.com>
2019-06-29 21:04:47 +0900

    if문 사용하기

---
+++ test_jdk/IfExample.java
@@ -0,0 +1,15 @@
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 22;
+		
+		if(age >= 20) {
+			System.out.println("당신은 음주 가능합니다 !");
+		}else {
+			System.out.println("미성년자 안돼 ~");
+		}
+	}
+
+}
