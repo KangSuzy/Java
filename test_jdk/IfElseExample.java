8d83aa2cf84c128e1b62b1e8ce74cdf3354a5a3e
KangSuzy <astnwl321@gmail.com>
2019-06-29 21:09:43 +0900

    if-else문 사용하기

---
+++ test_jdk/IfElseExample.java
@@ -0,0 +1,17 @@
+
+public class IfElseExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double score = 4.5;
+		
+		if (score>=4.5) {
+			System.out.println("당신 천재지");
+		}else if (score >=3.5) {
+			System.out.println("당신 잘했어");
+		}else {
+			System.out.println("당신 ^^");
+		}
+	}
+
+}
